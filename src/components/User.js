fc1773f6076b3eb0083a0b06170af30e2b6ba2ff
SatishPatil <satishsp1625@gmail.com>
2022-07-31 16:02:22 +0530

    Fetch API Consume

--- src/components/User.js
+++ src/components/User.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function User() {
-  return (
-    <div>
-      <h1>I am User Component</h1>
-    </div>
-  )
+    const [users, setUsers] = useState([])
+
+    const URL = "https://reqres.in/api/users";
+
+    useEffect(() => {
+        fetch(URL).then((resp => resp.json())).then(data => {
+            setUsers(data.data)
+            console.log(data.data)
+        })
+    }, [])
+
+    const dataUsers = users.map(res => {
+        return (
+            <tr key={res.id}>
+                <td>{res.id}</td>
+                <td>{res.email}</td>
+            
+            </tr>)
+    })
+
+    return (
+        <>
+            <h1>All User Info</h1>
+            <hr />
+            <table className='table'>
+                <thead>
+                    <th>Id</th>
+                    <th>EmailId</th>
+                </thead>
+                <tbody>
+                    {dataUsers}
+                </tbody>
+            </table>
+        </>
+    )
 }
 
 export default User
